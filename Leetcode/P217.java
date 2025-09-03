e3c6b87d15c0b7369f1ab674f14da213fdf01c69
Sneha-6392 <sneha.mauryasm02@gmail.com>
2025-09-03 09:28:48 +0530

    leetcode

---
+++ Leetcode/P217.java
@@ -0,0 +1,17 @@
+package CP.Leetcode;
+// 217. Contains Duplicate
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i : nums) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+        for (int i : freq.values()) {
+            if (i >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
