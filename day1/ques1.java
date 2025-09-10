001d5b5eedf8688657702bc53163dd9456ebff11
Sneha-6392 <sneha.mauryasm02@gmail.com>
2025-09-10 11:02:34 +0530

    core java

--- day1/ques1.java
+++
@@ -1,8 +0,0 @@
-// Welcome to Bridgelabz!
-package CP.day1;
-public class ques1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz!");
-    }
-    
-}
