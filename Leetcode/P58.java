7b6f13262d7aec218185c803886a3d2690378ceb
Sneha-6392 <sneha.mauryasm02@gmail.com>
2025-09-15 08:18:52 +0530

    java updated

--- Leetcode/P58.java
+++
@@ -1,9 +0,0 @@
-package CP.Leetcode;
-// 58. Length of Last Word
-class Solution {
-    public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        String str = arr[arr.length-1];
-        return str.length();
-    }
-}
